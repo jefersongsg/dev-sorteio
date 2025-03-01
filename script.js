cd9b5e61b168dc39687d23490b614ea489a5e017
jefersongsg <jefersonsilva5525@gmail.com>
2025-03-01 15:41:00 -0300

    alteraçao no styles e resultado mostrando na tela alert se colocar numeros menores do que o sorteador for sortear mudança de logo

--- script.js
+++ script.js
@@ -5,7 +5,14 @@ function generateNumber() {
 
     const result = Math.floor(Math.random() * (two - one + 1)) + one;
 
-    alert(result)
+    
+    if(result < two && result > one){
+        const resultadoInput = document.querySelector(".resultado");
+        resultadoInput.value = result;
+        resultadoInput.style.display = "block";
+    }else{
+        alert("insira um número válido");
+    }
 }
 
 
